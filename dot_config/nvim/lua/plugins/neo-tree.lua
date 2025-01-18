59e3f1115d5ef154774014d7f676ae9e06fd4da7
Yuvaraj <Yuvaraj.Selvarajan@shell.com>
2025-01-18 14:03:02 +0530

    feat: added neo-tree configs

---
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function()
+            vim.opt.relativenumber = true
+          end,
+        },
+      },
+    })
+  end,
+}
--- dot_config/nvim/lua/plugins/nvim-lualine.lua
+++ dot_config/nvim/lua/plugins/nvim-lualine.lua
@@ -43,23 +43,21 @@ return {
       },
       sections = {
         lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-        lualine_b = { "filename", "branch" },
-        lualine_c = {
-          "%=", --[[ add your center compoentnts here in place of this comment ]]
-        },
-        lualine_x = {},
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat" },
         lualine_y = { "filetype", "progress" },
         lualine_z = {
           { "location", separator = { right = "" }, left_padding = 2 },
         },
       },
       inactive_sections = {
-        lualine_a = { "filename" },
+        lualine_a = {},
         lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
-        lualine_z = { "location" },
+        lualine_z = {},
       },
       tabline = {},
       extensions = {},
