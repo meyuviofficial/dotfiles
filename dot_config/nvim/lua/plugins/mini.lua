237bd85f163ec63dbed32f2342aec134dd1eafec
Yuvaraj S <yuvarajselva@hotmail.com>
2025-06-16 13:38:11 +0530

    re-added the latest config files

---
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+  "echasnovski/mini.nvim",
+  version = "*",
+  name = "mini",
+  config = function()
+    require("mini.surround").setup() -- helps surrounding with quotes, brackets, etc.
+    require("mini.ai").setup() -- helps with a, i motion
+    require("mini.tabline").setup() -- helps with tabline navigation
+    require("mini.move").setup() -- helps with moving lines and blocks
+    require("mini.bracketed").setup() -- helps with bracketed text objects
+    require("mini.comment").setup() -- helps with commenting lines
+    require("mini.jump").setup() -- helps with jumping to definitions
+  end,
+}
