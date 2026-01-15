e60c01571a4d4351ff7fd987715eed2ccc805993
Yuvaraj S <ys+gwre@guidewire.com>
2026-01-15 16:32:37 +0530

    refactor: cleaned up the nvim setup

--- dot_config/nvim/lua/plugins/catppuccin-colorscheme.lua
+++ dot_config/nvim/lua/plugins/catppuccin-colorscheme.lua
@@ -1,1 +1,21 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- make sure it loads before everything else
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      integrations = {
+        treesitter = true,
+        native_lsp = {
+          enabled = true,
+        },
+        cmp = true,
+        gitsigns = true,
+        telescope = true,
+        which_key = true,
+        notify = true,
+      },
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/github-copilot.lua
+++ dot_config/nvim/lua/plugins/github-copilot.lua
@@ -1,5 +1,18 @@
 return {
-  "github/copilot.vim",
-  name = "copilot",
-  priority = 1001,
+  {
+    "github/copilot.vim",
+    event = "InsertEnter", -- load only when you actually type
+    config = function()
+      -- Optional but recommended sane defaults
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+
+      -- Use Ctrl+l to accept suggestions
+      vim.keymap.set("i", "<C-l>", 'copilot#Accept("<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+        silent = true,
+      })
+    end,
+  },
 }
