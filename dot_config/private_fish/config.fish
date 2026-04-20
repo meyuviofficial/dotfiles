c85b881ab4d13f6cfbe395ca4edf25cc27a3dc0f
Yuvaraj S <ys@apac-hc4nyghpqf.guidewire.com>
2026-04-20 14:56:46 +0530

    chore: updated the configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,24 +1,25 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-catppuccin_tide mocha rainbow
+# Commands to run in interactive sessions can go here
+# catppuccin_tide mocha rainbow
 
 # set vi mode
-fish_vi_key_bindings
+  fish_vi_key_bindings
 
 # fzf config
-fzf --fish | source
+  fzf --fish | source
 
 # backward kill a word
 # bind alt-backspace backward-kill-word
-bind -M insert alt-backspace backward-kill-word
+  bind -M insert alt-backspace backward-kill-word
 
 # Append path variable
-fish_add_path --append $HOME/go/bin
+  fish_add_path --append $HOME/go/bin
 
 # zoxide config
-zoxide init fish | source
+  zoxide init fish | source
 
 # Initialize Starship prompt
-# starship init fish | source
+  starship init fish | source
+end
+
+
