8b1aa800e3e3637fb00c4c11323b6a2c2b3f26a8
Yuvaraj S <yuvarajselva@hotmail.com>
2026-04-21 18:46:56 +0530

    feat: added fish aliases for kubectl & git

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,34 +1,33 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
-# catppuccin_tide mocha rainbow
+    # Commands to run in interactive sessions can go here
+    # catppuccin_tide mocha rainbow
 
-# set vi mode
-  fish_vi_key_bindings
+    # set vi mode
+    fish_vi_key_bindings
 
-# fzf config
-  fzf --fish | source
+    # fzf config
+    fzf --fish | source
 
-# backward kill a word
-# bind alt-backspace backward-kill-word
-  bind -M insert alt-backspace backward-kill-word
+    # backward kill a word
+    # bind alt-backspace backward-kill-word
+    bind -M insert alt-backspace backward-kill-word
 
-# Append path variable
-  fish_add_path --append $HOME/go/bin
-  fish_add_path --append /opt/homebrew/bin
-  fish_add_path --append '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
-  
-# zoxide config
-  zoxide init fish | source
+    # Append path variable
+    fish_add_path --append $HOME/go/bin
+    fish_add_path --append /opt/homebrew/bin
+    fish_add_path --append '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
+    fish_add_path --append /Users/ys/.local/bin
 
-# Initialize Starship prompt
-  starship init fish | source
+    # zoxide config
+    zoxide init fish | source
 
-# Universal variables
-  set -Ux XDG_CONFIG_HOME "$HOME/.config"
-  set -Ux HOMEBREW_CASK_OPTS "--appdir=~/Applications"
+    # Initialize Starship prompt
+    starship init fish | source
 
-# Global variables
-  set -gx NODE_EXTRA_CA_CERTS ~/.config/certs/zscaler.pem
+    # Universal variables
+    set -Ux XDG_CONFIG_HOME "$HOME/.config"
+    set -Ux HOMEBREW_CASK_OPTS "--appdir=~/Applications"
+
+    # Global variables
+    set -gx NODE_EXTRA_CA_CERTS ~/.config/certs/zscaler.pem
 end
-
-
